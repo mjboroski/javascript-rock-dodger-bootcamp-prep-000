a62ce09e63be90ead83f9e87f35b9fa3f2d22cf8
Michael Boroski <mjboroski@gmail.com>
2018-04-05 21:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function moveDodger(e) {
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 4}px`
   }
@@ -131,9 +131,9 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
- 
+
   if (right > 0) {
-    dodger.style.right = `${right - 4}px`
+    dodger.style.right = `${right + 4}px`
   }
 }
 
